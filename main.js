d6e2cb6acb74492c9d04e9fcf3f93c911ffb45b3
abdishakour1234 <154619799+abdishakour1234@users.noreply.github.com>
2023-12-22 19:36:34 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+/*let element = document.query.selector("h1");
+
+let element2 = document.query.selector("#element2");
+let element3 = document.query.selector(".element3");
+
+
+console.log(element);
+console.log(element2);
+console.log(element3);*/
+/*
+let main = document.querySelector("#main")
+
+let NewElement = document.createElement("p");
+let Newcontent = document.createTextNode("Welcome To THE NEW Element");
+NewElement.append(Newcontent);
+
+let NewElement2 = document.createElement("p");
+let Newcontent2 = document.createTextNode("Welcome To THE NEW Element2");
+NewElement2.appendChild(Newcontent2);
+
+
+
+
+main.append(NewElement);
+main.append(NewElement2);
+NewElement2.remove();
+console.log(NewElement);  */
+/*
+//let div = document.querySelector("div");
+//div.classList.add("success");
+//div.classList.remove("success");
+//div.classList.add("error");
+//p.style.fontSize = "40px"; */
+/*let button = document.querySelector("button");
+button.addEventListener("click", ()=>{
+    alert("clicked");
+});*/
+
+let typing = document.getElementById("typing");
+let clone = document.querySelector("#clone");
+
+
+
+console.log("Hello World!");
+
+
+
+console.log("hhhhhhhhhhhhhhhhhhhhhhhhhhhh");
+
+
